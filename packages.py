9d332f277a0b35a0ffe35c488c71675eaf7cc445
syeon8344 <yeon8344@gmail.com>
2024-09-06 18:17:36 +0900

    Initial commit

---
+++ packages.py
@@ -0,0 +1,11 @@
+# 설치할 패키지들 목록
+# 마우스로 클릭 > 빨간 전구 아이콘 > 패키지 ~~~ 설치
+from flask import Flask
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+import matplotlib.pyplot as plt
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from wordcloud import WordCloud, STOPWORDS
